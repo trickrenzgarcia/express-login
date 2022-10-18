4436dab4d2a0878da5d446494ec09455b84fb8dc
trickrenzgarcia <trickrenzgarcia@gmail.com>
2022-10-18 18:59:31 +0800

    initialize

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+const PORT = 5000
+const { uuid } = require('uuidv4')
+const { hash, compare } = require('bcrypt')
+const db = require('./database')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+app.get('/', (req, res) => {
+  res.send("hello")
+})
+
+app.get('/users', (req, res) => {
+  db.query(`SELECT * FROM users`, (err, results) => {
+    if (err) {
+      res.send({message: err})
+    }
+
+    res.status(200).send(results)
+  })
+})
+
+app.post('/register', (req, res) => {
+  const uid = uuid()
+  const { username, email, password } = req.body
+  hash(password, 10, (err, hashed) => {
+    db.query(`INSERT INTO users (uid, username, email, password) VALUES ('${uid}', '${username}', '${email}', '${hashed}')`, (err, result) => {
+      if (err) throw err;
+      console.log("1 user successfully added!")
+    })
+  })
+  res.send("done")
+})
+
+app.post('/login', (req, res) => {
+  const { email } = req.body;
+  const pass = req.body.password;
+  db.query(`SELECT * FROM users WHERE email='${email}'`, (err, result) => {
+    var qPass = result[0].password
+    var qEmail = result[0].email
+    var qUser = result[0].username
+    var qUid = result[0].uid
+    compare(pass, qPass, (error, result) => {
+      if(result) {
+        res.send({
+          loginStatus: result,
+          uid: qUid,
+          email: qEmail,
+          username: qUser,
+          password: qPass
+        })
+      }
+      else res.send("wrong password")
+    })
+  })
+  
+})
+
+app.listen(PORT, () => {
+  console.log(`the server is ready at http://localhost:${PORT}`)
+})
---
+++ database.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql')
+
+module.exports = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'api',
+  port: '3306'
+})
